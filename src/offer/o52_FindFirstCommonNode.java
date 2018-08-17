388424322fd132390738ef3c3ac6c929fa3feb27
zhangbo <1978533265@qq.com>
2018-08-17 21:04:27 +0800

    commit

--- src/offer/o52_FindFirstCommonNode.java
+++ src/offer/o52_FindFirstCommonNode.java
@@ -4,39 +4,18 @@ package offer;
  * 题目描述：输入两个链表，找出它们的第一个公共结点。（单向链表）
  * 思路：遍历两个链表得到它们的长度a和b，长度差为h，长链表先走h个距离，然后比较pHead1 == pHead2，如果不等，两个链表
  * 各走一步接着比较，一直到相同为止，否则返回null。
+ * 优化：解法如下，其实不用先遍历两个链表
  */
 public class o52_FindFirstCommonNode {
     public ListNode findFirstCommonNode(ListNode pHead1, ListNode pHead2) {
-        if(pHead1==null || pHead2==null)
+        if(pHead1 == null || pHead2 == null)
             return null;
-        int high = 0;
-        int low = 0;
-        ListNode p1 = pHead1;
-        ListNode p2 = pHead2;
-        while(p1.next != null) {
-            high++;
-            p1 = p1.next;
+        ListNode a = pHead1;
+        ListNode b = pHead2;
+        while(a != b){
+            a = a == null? pHead2 : a.next;
+            b = b == null? pHead1 : b.next;
         }
-        while(p2.next != null) {
-            low++;
-            p2 = p2.next;
-        }
-        if(high>=low){
-            for(int i=0;i<high-low;i++)
-                pHead1 = pHead1.next;
-            while(pHead1 != pHead2){
-                    pHead1 = pHead1.next;
-                    pHead2 = pHead2.next;
-            }
-        }
-        else if(high<low){
-            for(int i=0;i<low-high;i++)
-                pHead2 = pHead2.next;
-            while(pHead1 != pHead2){
-                    pHead1 = pHead1.next;
-                    pHead2 = pHead2.next;
-            }
-        }
-        return pHead1;
+        return a;
     }
 }
