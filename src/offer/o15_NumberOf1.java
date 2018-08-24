dec1947aa0d78364c8e7339387b02d926e38be9e
zhangbo <1978533265@qq.com>
2018-08-24 13:53:54 +0800

    commit

--- src/offer/o15_NumberOf1.java
+++ src/offer/o15_NumberOf1.java
@@ -1,7 +1,8 @@
 package offer;
 
 /**
- * 题目描述：输入一个整数，输出该数二进制表示中1的个数，其中负数用补码表示。
+ * 题目描述：二进制中1的个数
+ * 输入一个整数，输出该数二进制表示中1的个数，其中负数用补码表示。
  */
 public class o15_NumberOf1 {
     public int NumberOf1(int n) {
