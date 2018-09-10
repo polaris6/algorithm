e0d90326e637fe6c92a3b69579999b3244ccc449
zhangbo <1978533265@qq.com>
2018-09-10 17:04:46 +0800

    commit

--- src/leetcode/o746_MinCostClimbingStairs.java
+++ src/leetcode/o746_MinCostClimbingStairs.java
@@ -10,7 +10,7 @@ public class o746_MinCostClimbingStairs{
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
         for(int i = 2;i < n;i++)
-            cost[i] += Math.min(cost[i-1], cost[i-2]);
+            cost[i] += Math.min(cost[i-1], cost[i-2]);  //当人位于第i个台阶时，到第i个台阶的最小花费
         return Math.min(cost[n-1], cost[n-2]);
     }
 }
